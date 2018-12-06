4dff984eda7e63f5d4dce20c5d01abcf104d01a1
Tanguy BERNAERT <tanguy.bernaert@gmail.com>
2018-12-06 14:10:20 +0100

    adding the firebase resources

--- app/src/main/java/com/projeta3/user/supermarket/Liste_Plan_Activity.java
+++ app/src/main/java/com/projeta3/user/supermarket/Liste_Plan_Activity.java
@@ -2,12 +2,15 @@ package com.projeta3.user.supermarket;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Liste_Plan_Activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste__plan_);
+
     }
 }
--- app/src/main/java/com/projeta3/user/supermarket/MainActivity.java
+++ app/src/main/java/com/projeta3/user/supermarket/MainActivity.java
@@ -74,9 +74,12 @@ public class MainActivity extends AppCompatActivity {
             }
             else {
                 Toast.makeText(this, result.getContents(),Toast.LENGTH_LONG).show();
+
+                Intent Liste_Plan_Activity = new Intent(MainActivity.this, Liste_Plan_Activity.class);
+                startActivity(Liste_Plan_Activity);
+
                 text_scanned = result.getContents();
                 textView.setText(text_scanned);
-
             }
         }
         else {
@@ -85,11 +88,13 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
 
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+        if (requestCode == MY_CAMERA_REQUEST_CODE)
+        {
 
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
@@ -104,67 +109,3 @@ public class MainActivity extends AppCompatActivity {
         }}//end onRequestPermissionsResult
 }
 
-
-/*
-import android.app.Activity;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-
-public class MainActivity extends AppCompatActivity {
-
-    private Button scan_btn;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        scan_btn = (Button) findViewById(R.id.scan_button);
-        final Activity activity = this;
-        scan_btn.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View view) {
-                IntentIntegrator integrator = new IntentIntegrator(activity);
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
-                integrator.setPrompt("Scan");
-                integrator.setCameraId(0);
-                integrator.setBeepEnabled(false);
-                integrator.setBarcodeImageEnabled(false);
-
-
-                Intent Affichage_Activity = new Intent(MainActivity.this, Affichage_Activity.class);
-                startActivity(Affichage_Activity);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-
-        if(result != null)
-        {
-            if(result.getContents()==null)
-            {
-                Toast.makeText(this, "Vous avez arretez le scan du QR-Code", Toast.LENGTH_LONG).show();
-            }
-            else
-                {
-                    Toast.makeText(this, result.getContents(),Toast.LENGTH_LONG).show();
-                }
-        }
-        else
-            {
-                super.onActivityResult(requestCode, resultCode, data);
-            }
-    }
-}*/
